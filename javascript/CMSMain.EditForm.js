805d01453182eaea8f971e563284c7f0838f8fb2
Kirk Mayo <kirk@silverstripe.com>
2012-12-06 10:50:16 +1300

    BUG ClassName was causing form to be saved when clicked and nothing changed trac 7876

--- javascript/CMSMain.EditForm.js
+++ javascript/CMSMain.EditForm.js
@@ -11,12 +11,48 @@
 		$('.cms-edit-form :input[name=ClassName]').entwine({
 			// Function: onchange
 			onchange: function() {
-				if (confirm(ss.i18n._t('CMSMAIN.CONFIRMSAVECLASSNAME'))) {
-					$('[name=action_save]').click();
-				} else {
-					return false;
+
+				var origVal = this.data('changetracker.origVal');
+				var currentVal = $(this).val();
+				if (origVal == currentVal) {
+					return;
 				}
+
+				// the main form and ClassName will have the class changed applied
+				if ($('.changed').length < 3) {
+					$('[name=action_save]').click();
+					return;
+				}
+				var save = ss.i18n._t('URLSEGMENT.Savce & Continue', 'Save & Continue');
+				var cancel = ss.i18n._t('URLSEGMENT.Cancel', 'Cancel');
+				var title = ss.i18n._t('URLSEGMENT.CONFIRMSAVECLASSNAME',
+					'Do you want to update the page type?')
+				var dialog = $('<div id="savePageType" title="' + title + '"/>');
+				$('body').append(dialog);
+				$('#savePageType').dialog({
+					resizable: false,
+					height:140,
+					modal: true,
+					buttons: [
+						{
+							text: save,
+							click: function() {
+								$( this ).dialog('close');
+								$('[name=action_save]').click();
+							},
+						},
+						{
+							text: cancel,
+							click : function() {
+								$( this ).dialog('close');
+								return false;
+							}
+						}
+					]
+				});
+				$('#savePageType').dialog('open');
 			}
+
 		});
 
 		/**
